d3cdd7b16ff003f0d05db5f7e6020033f0fd2408
Bruno Sanguineti <sanguineti.bruno@pwc.com>
2020-10-18 20:57:55 -0300

    Adding Customers Properties

--- Acme.BusinessLayer/Customer.cs
+++ Acme.BusinessLayer/Customer.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Acme.BusinessLayer
 {
-    public class Class1
+    class Customer
     {
+        public int CustomerId { get; private set; } // 2- propg tab tab shortkey.
+        public string EmailAddress  { get; set; } // 1- Insert Prop Snippet ctrl+k, ctrl x
+        public string FirstName {get; set;} // Auto Implemented Property.
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return LastName + "," + FirstName;
+            }
+        }
+            
+            
+
     }
 }
