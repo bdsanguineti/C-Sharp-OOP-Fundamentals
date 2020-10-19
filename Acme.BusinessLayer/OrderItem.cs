038d80f402299cbeebc428ba5b992757e3219bef
Bruno Sanguineti <sanguineti.bruno@pwc.com>
2020-10-19 14:59:21 -0300

    Adding clases Order, Order Itel and Products

---
+++ Acme.BusinessLayer/OrderItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.BusinessLayer
+{
+    class OrderItem
+    {
+        public OrderItem()
+        {
+
+        }
+        public OrderItem(int orderItemId)
+        {
+            orderItemId = orderItemId;
+        }
+        public int OrderItemId { get; private set; }
+        public string ProductId { get; private set; }
+        public decimal? PurchasePrice { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
---
+++ Acme.BusinessLayer/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.BusinessLayer
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+        public Product(int productId)
+        {
+            ProductId = productId;
+        }
+        public decimal? CurrentPrice { get; set; }
+        public string ProductDescription { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+
+        // Retrieve one Product
+        public Product Retrieve(int productId)
+        {
+            return new Product();
+        }
+
+        // Saves the current product
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+            if (string.IsNullOrWhiteSpace(ProductName)) isValid = false;
+            if (CurrentPrice == null) isValid = false;
+
+            return isValid;
+        }
+        
+    }
+}
