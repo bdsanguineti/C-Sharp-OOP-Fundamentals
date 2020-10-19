0150ad2747f5ab0eddc312ffdac37c4a6aa2fa11
Bruno Sanguineti <sanguineti.bruno@pwc.com>
2020-10-19 10:56:00 -0300

    overloading constructor

--- Acme.BusinessLayer/Customer.cs
+++ Acme.BusinessLayer/Customer.cs
@@ -8,6 +8,17 @@ namespace Acme.BusinessLayer
 {
     public class Customer
     {
+        // Two constructors with the same name and differents parammeters (overloading the constructor)
+        // We need to define the default constructor as well, otherwise we wont be able to create
+        // A instance of the object without the parameter.
+        public Customer() // 
+        {
+            
+        }
+        public Customer(int customerId) // Snipped ctor tabtab
+        {
+            CustomerId = CustomerId;
+        }
         public int CustomerId { get; private set; } // 2- propg tab tab shortkey.
         public string EmailAddress  { get; set; } // 1- Insert Prop Snippet ctrl+k, ctrl x
         public string FirstName {get; set;} // Auto Implemented Property.
@@ -32,5 +43,35 @@ namespace Acme.BusinessLayer
         // Use the Static Modifier to declare members that belong to the class itself
         // And not to an instance of that class.
         public static int InstanceCount { get; set; }
+
+        // Validates the customer data.
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAddress)) isValid = false;
+
+            return isValid;
+        }
+
+        public Customer Retrieve(int CustomerId)
+        {
+            // Code that retrives the defined customer
+            return new Customer();
+        }
+
+        // Retrieve all the customers
+        public List<Customer> Retrieve()
+        {
+            // Code that retrieves all of the Customers
+            return new List<Customer>();
+        }
+
+        public bool Save()
+        {
+            // Code that save the defined customer
+            return true;
+        }
     }
 }
