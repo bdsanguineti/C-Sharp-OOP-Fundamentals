92c68b3160b446713997e1c02380423bbbc36cd5
Bruno Sanguineti <sanguineti.bruno@pwc.com>
2020-10-19 22:14:38 -0300

    Adding Test for Customer Repository Class

---
+++ Acme.BusinessLayerTest/CustomerRepositoryTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Acme.BusinessLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acme.BusinessLayerTest
+{
+    [TestClass]
+    public class CustomerRepositoryTest
+    {
+        [TestMethod]
+        public void RetrieveValid()
+        {
+            // Arrange
+            var customerRepository = new CustomerRepository();
+            var expected = new Customer(1)
+            {
+                EmailAddress = "kennybell@gmail.com",
+                FirstName = "Kenny",
+                LastName = "Bell"
+            };
+
+            // Act
+            var actual = customerRepository.Retrieve(1);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
