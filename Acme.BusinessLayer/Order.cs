b0598d76a88b01799a98b5ac8cea8870f13a75f1
Bruno Sanguineti <sanguineti.bruno@pwc.com>
2020-10-20 14:43:08 -0300

    Composition ids

--- Acme.BusinessLayer/Order.cs
+++ Acme.BusinessLayer/Order.cs
@@ -8,17 +8,21 @@ namespace Acme.BusinessLayer
 {
     public class Order
     {
-        public Order()
+        public Order() : this(0)
         {
 
         }
         public Order(int orderId)
         {
             orderId = orderId;
+            OrderItems = new List<OrderItem>();
         }
 
+        public int CustomerId { get; set; }
         public DateTimeOffset? OrderDate { get; set; }
         public int OrderId { get; private set; }
+        public List<OrderItem> OrderItems { get; set; }
+        public int ShippingAddressId { get; set; }
         public bool Validate()
         {
             var isValid = true;
--- Acme.BusinessLayer/OrderItem.cs
+++ Acme.BusinessLayer/OrderItem.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Acme.BusinessLayer
 {
-    class OrderItem
+    public class OrderItem
     {
         public OrderItem()
         {
